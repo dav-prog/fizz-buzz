7a07b7fdf7f02f187441d8132fd28b04c4d423de
David Trimmer <dbtrimmer1@gmail.com>
2020-09-15 13:50:06 -0400

    FizzBuzz interpret method finished

---
+++ src/main/java/project/FizzBuzz.java
@@ -0,0 +1,16 @@
+package project;
+
+public class FizzBuzz {
+    public String interpret(int numberToSay) {
+        if (numberToSay % 3 == 0 && numberToSay % 5 == 0){
+            return "FizzBuzz";
+        }
+        if (numberToSay % 3 == 0){
+            return "Fizz";
+        }
+        if (numberToSay % 5== 0){
+            return "Buzz";
+        }
+        return "" + numberToSay;
+    }
+}
---
+++ src/test/java/project/FizzBuzzTest.java
@@ -0,0 +1,84 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void shouldBeAbleToCreatFizzBuzzObject(){
+       //Arrange and Action
+        FizzBuzz underTest = new FizzBuzz();
+        //Assertion
+        assertTrue(underTest != null);
+    }
+
+    @Test
+    public void given1ShouldReturn1(){
+        //Arrange
+        FizzBuzz underTest = new FizzBuzz();
+
+        //Action
+        String result = underTest.interpret(1);
+
+        //Assertion
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void given2ShouldReturn2(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(2);
+
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void given3ShouldReturnFizz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(3);
+
+        assertEquals("Fizz", result);
+
+    }
+    @Test
+    public void given5ShouldReturnBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(5);
+
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void given6ShouldReturnFizz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(6);
+
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void given10ShouldReturnBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(10);
+
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void given15ShouldReturnFizzBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(15);
+
+        assertEquals("FizzBuzz", result);
+    }
+
+}
